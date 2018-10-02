06db071ae778233ecb7bf4cbb63fda3e61e5a8af
Niklas Schweizer <dumblexx@gmail.com>
2018-10-02 14:01:19 +0200

    La till TODO kommentarer

---
+++ src/js/memory.js
@@ -0,0 +1,2 @@
+/* TODO: 1. Ett fungerande memory-spel ska skapas som använder sig utav de bilder som finns ibland filerna.
+Spelet ska har brickor uppdelade i 4 rader, med 4 brickor på varje rad. */
